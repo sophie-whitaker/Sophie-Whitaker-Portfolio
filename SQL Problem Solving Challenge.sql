e0048e0e1a88adc6ffcbe818234cb0fc963a13cf
Sophie Whitaker <soph.whitaker@outlook.com>
2024-07-13 21:23:05 +0100

    Create SQL Problem Solving Challenge.sql

---
+++ SQL Problem Solving Challenge.sql
@@ -0,0 +1,47 @@
+/* You are given two tables, 'flights' and 'airports'.  
+Each row in the table 'flights' contains information about a flight: code of departure pore (start_port) and code of destination port (end_port).
+Each row in the table 'airports' contains information about an airport:the city name (city_name) and the port code (port_code).  Each port_code is assigned to at most one airport.
+Write an SQL query that finds all cities through which a flight from New York to Tokyo may pass if the passenger wants to make exactly one change of flights.  
+*/
+
+SELECT 
+    start_port, 
+    end_port
+FROM 
+    flights;
+
+SELECT * FROM flights WHERE start_port='JFK';
+
+SELECT * FROM airports WHERE city_name='New York';
+SELECT * FROM airports WHERE city_name='Tokyo';
+
+-- from end_port WHERE start_port='JFK';
+
+CREATE TABLE starting_airports(
+-- ID  integer PRIMARY KEY AUTOINCREMENT,
+name    char(3)
+);
+
+INSERT INTO starting_airports(name) SELECT port_code FROM airports WHERE city_name='New York';
+
+CREATE TABLE ending_airports(
+-- ID  integer PRIMARY KEY AUTOINCREMENT,
+name    char(3)
+);
+
+INSERT INTO ending_airports(name) SELECT port_code FROM airports WHERE city_name='Tokyo';
+
+CREATE TABLE intermediary_airports(
+-- ID  integer PRIMARY KEY AUTOINCREMENT,
+name    char(3)
+);
+
+INSERT INTO intermediary_airports(name) SELECT end_port FROM flights WHERE start_port IN starting_airports;
+
+CREATE TABLE airports_destinationtokyo(
+name    char(3)
+);
+
+INSERT INTO airports_destinationtokyo(name) SELECT start_port FROM flights WHERE end_port IN ending_airports AND start_port IN intermediary_airports;
+
+SELECT city_name FROM airports WHERE port_code IN airports_destinationtokyo;
